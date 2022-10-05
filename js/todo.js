239d87c5b9f6f8dbfc87a65d9e95224cb4eb6bb5
Nate J.M <nate@natejonah.com>
2022-10-05 09:26:43 +0100

    Work on todo list

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,10 @@
-console.log('todo!');
+const newTask = getElementById("newTask");
+
+/*
+Icons to use
+
+https://icons.getbootstrap.com/icons/circle/
+https://icons.getbootstrap.com/icons/check-circle-fill/
+https://icons.getbootstrap.com/icons/check-circle/
+
+*/
