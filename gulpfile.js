67d09b560c294905e4a302659396017bc2303d9b
Nate J.M <nate@natejonah.com>
2022-10-13 21:23:57 +0100

    Create SCSS gulp task
    Also renames JS task and adds both to default

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
+"use strict";
+
 const { dest, parallel, series, src } = require("gulp");
 const babel = require("gulp-babel");
+const sass = require('gulp-sass')(require('sass'));
 
-function defaultTask() {
+function js() {
     return src("src/js/app.js")
         .pipe(babel())
         .pipe(dest("dist/js"));
 }
 
+function scss() {
+    return src("src/scss/app.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(dest("dist/css"));
+}
+
 module.exports = {
-    default: defaultTask
+    default: series(scss, js),
+    sass: scss,
+    js: js
 };
