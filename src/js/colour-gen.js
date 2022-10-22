8eb5901079e8d36a376ad3257f02dfaa536822c0
Nate J.M <nate@natejonah.com>
2022-10-22 11:39:35 +0100

    Move Colour Gen JS to src folder
    Also changes colour box element

--- src/js/colour-gen.js
+++ src/js/colour-gen.js
@@ -1,10 +1,10 @@
 const button = document.getElementById("generate");
 
-const getColour = () => Math.floor(Math.random() * 256);
-
 function generate() {
     const colourMessage = document.getElementById("colourMessage");
     colourMessage.textContent = "Your generated colour is:";
+
+    const getColour = () => Math.floor(Math.random() * 256);
 
     const r = getColour();
     const g = getColour();
@@ -13,11 +13,11 @@ function generate() {
 
     const hex = "#" + r.toString(16) + g.toString(16) + b.toString(16);
 
-    const body = document.getElementsByTagName("body")[0];
+    const colourBox = document.getElementsByClassName("colour-gen")[0];
     const rgbCode = document.getElementById("rgb");
     const hexCode = document.getElementById("hex");
 
-    body.style.backgroundColor = rgb;
+    colourBox.style.backgroundColor = rgb;
     rgbCode.textContent = rgb;
     hexCode.textContent = hex;
 }
